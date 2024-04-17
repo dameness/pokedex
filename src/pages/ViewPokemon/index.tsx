aee72ca99e570035ace1745e9be915a7731bf88b
Bernardo Damiani <bmdamiani17@gmail.com>
2024-04-17 14:32:21 -0300

    renomeando pasta de instancia do axios de  services para config

--- src/pages/ViewPokemon/index.tsx
+++ src/pages/ViewPokemon/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Pokemon } from "../../models/Pokemon";
 import { Stat } from "../../models/Stat";
 import { Ability } from "../../models/Ability";
-import api from "../../services/api";
+import api from "../../config/api";
 import FavContext from "../../contexts/FavContext";
 import Types from "../../components/Types";
 
