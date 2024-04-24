a6c53452f062c54ddc8d3253855585716b52da59
bernardo damiani <bmdamiani17@gmail.com>
2024-04-24 17:59:57 -0300

    header arrumado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,18 +8,21 @@ export default function Header() {
   const smallScreen = useMediaQuery({ maxWidth: 640 });
   return (
     <header className="flex items-center justify-between w-screen h-14 p-3 mb-4">
-      <Link
-        className="ml-4 px-1 flex items-center hover:scale-105 transition-all "
-        to="/"
-      >
-        <img className="w-8 min-w-8" src="/pokeball.png" alt="pokeball" />
-        <img
-          className="w-24 min-w-24 mb-1 -ml-0.5"
-          src="/pokedex.png"
-          alt="pokedex-logo"
-        />
-      </Link>
-      <DarkModeSwitch />
+      <div className="flex items-center">
+        <Link
+          className="ml-4 px-1 flex items-center hover:scale-105 transition-all "
+          to="/"
+        >
+          <img className="w-8 min-w-8" src="/pokeball.png" alt="pokeball" />
+          <img
+            className="w-24 min-w-24 mb-1 -ml-0.5"
+            src="/pokedex.png"
+            alt="pokedex-logo"
+          />
+        </Link>
+        <DarkModeSwitch />
+      </div>
+
       {smallScreen ? <NavDropdown /> : <NavList />}
     </header>
   );
