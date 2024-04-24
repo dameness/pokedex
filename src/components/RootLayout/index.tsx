a41905a21d4918d7ad78216b68eae3459a54c0c6
bernardo damiani <bmdamiani17@gmail.com>
2024-04-24 15:19:56 -0300

    começando dark mode

--- src/components/RootLayout/index.tsx
+++ src/components/RootLayout/index.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import { useContext } from "react";
+import DarkModeContext from "@/contexts/DarkModeContext";
 
 export default function RootLayout() {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div>
-      <Header />
-      <main className="container mx-auto p-2">
-        <Outlet />
-      </main>
+    <div className={`${darkMode && "dark"}`}>
+      <div className="h-full min-h-screen dark:bg-neutral-800">
+        <Header />
+        <main className="container mx-auto p-2">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
