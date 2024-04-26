d63d175638315d7bb6abdd883cd2d44970356aa7
bernardo damiani <bmdamiani17@gmail.com>
2024-04-26 18:36:13 -0300

    grid layout fix

--- src/components/PokemonGrid/index.tsx
+++ src/components/PokemonGrid/index.tsx
@@ -10,16 +10,18 @@ export default function PokemonGrid({ pokemonIds, isFavoritesGrid }: Props) {
     return <></>;
   }
   return (
-    <div
-      className={`xs:grid-cols-3 grid-cols-2  xs:gap-x-10 gap-y-4 gap-x-3 ${
-        pokemonIds.length > 2
-          ? "grid"
-          : "xs:flex grid  justify-center items-start"
-      } `}
-    >
-      {pokemonIds.map((id) => (
-        <Card key={id} pokemonId={id} isFavoritesGrid={isFavoritesGrid} />
-      ))}
+    <div className="flex flex-col justify-center items-center xs:text-base text-xs">
+      <div
+        className={`xs:grid-cols-3 grid-cols-2  xs:gap-x-10 gap-y-4 gap-x-3 ${
+          pokemonIds.length > 2
+            ? "grid"
+            : "xs:flex grid  justify-center items-start"
+        } `}
+      >
+        {pokemonIds.map((id) => (
+          <Card key={id} pokemonId={id} isFavoritesGrid={isFavoritesGrid} />
+        ))}
+      </div>
     </div>
   );
 }
