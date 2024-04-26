8aaa30b180a6fc5585e5190a83e040f9761de40c
bernardo damiani <bmdamiani17@gmail.com>
2024-04-26 15:45:22 -0300

    customização de favorites no header

--- src/components/Header/utils/NavList.tsx
+++ src/components/Header/utils/NavList.tsx
@@ -1,7 +1,10 @@
+import FavContext from "@/contexts/FavContext";
 import { Home, Search, Star } from "lucide-react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavList() {
+  const { favorites } = useContext(FavContext);
   return (
     <div className="font-extrabold flex items-center space-x-4 mr-4 text-xs xs:text-sm">
       <Link
@@ -15,7 +18,14 @@ export default function NavList() {
         to="/favorites"
         className="flex dark:text-neutral-200 dark:hover:text-neutral-300 text-slate-800 items-center hover:text-slate-600"
       >
-        <Star className="mr-1" size={17} />
+        <div className="relative">
+          {favorites.length > 0 && (
+            <div className="absolute -bottom-2 -left-2 text-[9px] bg-red-300 dark:text-black w-3.5 h-3.5 rounded-full flex items-center justify-center">
+              {favorites.length}
+            </div>
+          )}
+          <Star className="mr-1" size={17} />
+        </div>
         Favorites
       </Link>
       <Link
